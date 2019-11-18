7365671865130b3758d0c4b71d0a71234034751e
nickschiffer <nick@schiffer.us>
2019-11-17 17:32:46 -0800

    removed unnecessary files from nickscripts

--- MRL/MRL_PI/nickscripts/resource/Arduino/MrlComm/Device.cpp
+++
@@ -1,9 +0,0 @@
-#include "ArduinoMsgCodec.h"
-#include "Msg.h"
-#include "Device.h"
-
-Device::Device(byte deviceId, byte deviceType) {
-  id = deviceId;
-  type = deviceType;
-  msg = Msg::getInstance();
-}
